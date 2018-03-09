bc63d6c265fb0c96ae4b629a72c608a6878df942
Sean Hanlon <shanlon800@gmail.com>
2018-03-09 17:32:12 -0500

    add new product form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :websites, only: [:index]
+      resources :products, only: [:create]
     end
   end
 end
