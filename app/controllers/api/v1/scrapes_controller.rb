612738f205cda6b6ad6323c772ab74840976b5f2
Sean Hanlon <shanlon800@gmail.com>
2018-03-12 12:46:20 -0400

    created a button that runs the scrape when clicked

---
+++ app/controllers/api/v1/scrapes_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ScrapesController < ApplicationController
+  def index
+    products = Product.all
+    all_products_price_display = []
+    products.each do |product|
+      amazon_agent = Mechanize.new
+      amazon_page = amazon_agent.get("https://www.amazon.com/Safety-1st-Grow-Convertible-Everest/dp/#{product.amazon_asin}")
+      amazon_price = amazon_page.css('#priceblock_ourprice').text.delete('$ ,')
+      last_amz_price = Price.create(price_amt: amazon_price.to_f, product: product, website_id: 1)
+
+      walmart_agent = Mechanize.new
+      walmart_page = walmart_agent.get("https://www.walmart.com/ip/Safety-1-Safety-Essentials-Kit-46-Pcs-White/#{product.walmart_id}")
+      walmart_price = walmart_page.css('.Price-group')[0].text.delete('$ ,')
+
+      last_walmart_price = Price.create(price_amt: walmart_price.to_f, product: product, website_id: 2)
+      last_scrape = Scrape.create(product: product, walmart_price_amt: last_walmart_price.price_amt, walmart_price_id: last_walmart_price.id, amazon_price_amt: last_amz_price.price_amt, amazon_price_id: last_amz_price.id)
+      all_products_price_display << {product_name: product.product_name, amazon_price: last_amz_price.price_amt, walmart_price: last_walmart_price.price_amt, id: product.id}
+    end
+    render json: all_products_price_display
+  end
+end
